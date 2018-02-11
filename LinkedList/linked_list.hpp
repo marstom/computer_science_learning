199623f1a1a827957f018121c860c674dce49a3b
Tomasz Marszałek <marstomek@gmail.com>
2018-02-11 18:15:19 +0100

    Linked list 1

--- LinkedList/linked_list.hpp
+++ LinkedList/linked_list.hpp
@@ -1,17 +1,92 @@
 #include <iostream>
+using namespace std;
 
-size_t size();
+struct Node{
+    int data;
+    Node* next = NULL;
+};
+
+struct LinkedList{
+    Node* start;
+};
+
+//returns number of data elements in list
+size_t size(LinkedList *linkedList){
+    Node *el = linkedList->start;
+    size_t size = 0;
+    while(el){
+        size++;
+        el = el->next;
+    }
+    return size;
+}
+
+//  bool returns true if empty
 void empty();
+
+// returns the value of the nth item (starting at 0 for first)
 int value_at(int index);
-void push_front(int value);
+
+// adds an item to the front of the list
+void push_front(LinkedList *linkedList, int value);
+
+// remove front item and return its value
 void pop_front();
-void push_back(int value);
+
+// adds an item at the end
+void push_back(LinkedList *linkedList, int value){
+    Node* el = new Node();
+    el->data = value;
+    el->next = linkedList->start;
+    linkedList->start = el;
+}
+
+// removes end item and returns its value
 void pop_back();
+
+// get value of front item
 void front();
+
+// get value of end item
 void back();
+
+// insert value at index, so current item at that index is pointed to by new item at index
 void insert(int index,int value);
+
+// removes node at given index
 void erase(int index);
+
+// returns the value of the node at nth position from the end of the list
 int value_n_from_end(int n);
+
+// reverses the list
 void reverse();
-void remove_value(int value);
+
+// removes the first item in the list with this value
+void remove_value(LinkedList *linkedList, int value){
+    Node* el = linkedList->start;
+    while(el){
+        //if(el->next)
+        //Node* prev = el;
+        if(el->next && el->next->data == value){
+            Node* current = el;
+            Node* next = el->next;
+            current->next = next->next;
+        }
+        el=el->next;
+    }
+    //Node* ptr = el;
+    //el = el->next; //todo free memeory
+
+}
+
+// print all elements
+void print_whole_list(LinkedList* linkedList){
+    Node* el = linkedList->start;
+    while(el){
+        std::cout << el->data << ", ";
+        el=el->next;
+    }
+    std::cout << '\n';
+}
 
--- LinkedList/tests.cpp
+++ LinkedList/tests.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
 #include <string>
+#include <cassert>
+
 #include "linked_list.hpp"
+using namespace std;
 
 void test_add(){
+    LinkedList* aa = new LinkedList();
+    push_back(aa, 3);
+    push_back(aa, 12);
+    push_back(aa, 24);
+    push_back(aa, 1);
+    //  push_back(aa, 53);
+    print_whole_list(aa);
+}
+
+void test_size(){
+    LinkedList* aa = new LinkedList();
+    push_back(aa, 3);
+    push_back(aa, 12);
+    push_back(aa, 24);
+    push_back(aa, 1);
+    cout << size(aa) << endl;
+    assert (size(aa) == 4);
+}
+
+void test_remove_value(){
+    LinkedList* aa = new LinkedList();
+    push_back(aa, 1);
+    push_back(aa, 2);
+    push_back(aa, 3);
+    push_back(aa, 4);
+    remove_value(aa, 4);
+    print_whole_list(aa);
 
 }
 
 int main(){
-
-
+    test_add();
+    test_size();
+    test_remove_value();
     return 0;
 }

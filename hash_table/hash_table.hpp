9bb31403252a06adad2a4716c57f92f98ca9e91e
Tomasz Marszałek <marstomek@gmail.com>
2018-03-31 16:34:58 +0200

    HashTable add, exists remove + tests for it

--- hash_table/hash_table.hpp
+++ hash_table/hash_table.hpp
@@ -6,7 +6,9 @@
  */
 #pragma once
 #define ARRAY_SIZE 10
-
+#define LARGE_PRIME 443555
+#define _A 3214
+#define _B 4357
 #include <string>
 #include <vector>
 #include <stdexcept>
@@ -14,11 +16,75 @@
 
 
 template<typename T>
-struct Bucket{
+struct Node{
+    Node(){
+    }
+    Node(std::string k, T d){
+        key = k;
+        data = d;
+    }
     std::string key;
     T data;
-    Bucket* next;
+    Node* next = nullptr;
 
+};
+
+template<typename T>
+class Bucket{
+public:
+    Bucket(): root(nullptr){
+    }
+
+    void push_back(std::string key, T el){
+        if(root == nullptr){
+            root = new Node<T>();
+        }
+        if(root->key == key){
+            root->data = el;
+        }
+        else{
+            root->next = new Node<T>(key, el);
+        }
+    }
+
+    T find(std::string key){
+        Node<T>* element = root;
+        while(element != nullptr){
+            if(element->key == key){
+                return element->data;
+            }
+            element = element->next;
+        }
+        throw std::length_error("No such element");
+    }
+
+    void remove(std::string key){
+        Node<T>* element = root;
+        while(element != nullptr){
+            if(element != nullptr && element->next->key == key){
+                if(element->next != nullptr){
+                    // this is not last element
+                    element = element->next->next;
+                }
+                delete element->next;
+            }
+            element = element->next;
+        }
+        throw std::length_error("No such element");
+    }
+
+    bool exists(std::string key){
+        Node<T>* element = root;
+        while(element != nullptr){
+            if(element->key == key){
+                return true;
+            }
+            element = element->next;
+        }
+        return false;
+    }
+private:
+    Node<T> *root;
 };
 
 template<typename T>
@@ -26,65 +92,65 @@ class HashTable{
 public:
 
     HashTable(): num_buckets(ARRAY_SIZE){
-        buckets = new Bucket<T>*[ARRAY_SIZE];
-        for(int i = 0; i<ARRAY_SIZE; i++){
-            buckets[i] = new Bucket<T>();
-        }
+        buckets = new Bucket<T>[ARRAY_SIZE];
     }
 
     ~HashTable(){
-        // delete[] buckets;
-    }
-
-    int hash(std::string key){
-        // [(a*k+b) mod p] mod m
-        // a b -randof 0..p-1
-        // p big prime number, must be bigger than ARRAY_SIZE
-        // m table_size
-        int h = 0;
-        for(auto k: key){
-            h += ((3214*k+4357) % 443555) % ARRAY_SIZE;
-        }
-        return h % ARRAY_SIZE;
+         delete[] buckets;
     }
 
     void add(std::string key, T value){
         keys.push_back(key);
         int index = hash(key);
-        Bucket<T> *b = buckets[index];
-        while(b != nullptr){ // add value to bucket
-            if(b->key == key){
-                b->value = value;
-                return;
-            }
-            b= b->next;
-        }
-
+        buckets[index].push_back(key, value);
     }
 
     bool exists(std::string key){
-        return true;
+        // do it by hash TODO
+        for(size_t i=0; i<ARRAY_SIZE-1; i++){
+            if(buckets[i].exists(key))
+                return true;
+        }
+        return false;
     }
 
     T operator[](std::string key){
         int k = hash(key);
-        return buckets[k];
+        Bucket<T> b = buckets[k];
+        T el = b.find(key);
+        return el;
     }
     void remove(std::string key){
-
+        int k = hash(key);
+        Bucket<T> b = buckets[k];
+        b.remove(key);
     }
 
     void print_hash_table(){
 
     }
 
-    void add_bucket(); // helper functions
-    T get_bucket();
+    void add_bucket(){} // helper functions
+    T get_bucket(){}
     
 private:
 
+    int hash(std::string key){
+        /*
+            [(a*k+b) mod p] mod m
+            a b -randof 0..p-1
+            p big prime number, must be bigger than ARRAY_SIZE
+            m table_size
+        */
+        int h = 0;
+        for(auto k: key){
+            h += ((_A*k+_B) % LARGE_PRIME) % ARRAY_SIZE;
+        }
+        return h % ARRAY_SIZE;
+    }
+
     size_t num_buckets; // array size
-    Bucket<T> **buckets;
+    Bucket<T> *buckets;
     std::vector<std::string> keys;
 };
 
--- hash_table/unittest/unittest.cpp
+++ hash_table/unittest/unittest.cpp
@@ -9,24 +9,50 @@
 #include <gtest/gtest.h>
 #include "../hash_table.hpp"
 
+
+#define private public
+
 using namespace std;
 
 
-TEST(hash_table, hash){
-    HashTable<int> ht;
-    ht.hash("tomek");
-}
+//FRIEND_TEST(hash_table, hash){
+//    HashTable<int> ht;
+//    ht.hash("tomek");
+//}
 
 TEST(hash_table, add){
 
+    HashTable<int> ht;
+    ht.add("a", 232);
+    ht.add("b", 26);
+    ht.add("c", 232);
+    ht.add("d", 232);
+    ht.add("a", 7777);
+    cout << ht["a"] << endl;
 }
 
 TEST(hash_table, exists){
-
+    HashTable<int> ht;
+    ht.add("a", 232);
+    ht.add("b", 26);
+    ht.add("c", 232);
+    ht.add("d", 232);
+    ht.add("a", 7777);
+    ASSERT_FALSE(ht.exists("r"));
+    ASSERT_TRUE(ht.exists("a"));
+    ASSERT_TRUE(ht.exists("c"));
 }
 
 TEST(hash_table, remove){
-
+    HashTable<int> ht;
+    ht.add("a", 232);
+    ht.add("b", 26);
+    ht.add("c", 232);
+    ht.add("d", 232);
+    ht.add("a", 7777);
+    ASSERT_TRUE(ht.exists("c"));
+    ht.remove("c"); 
+    ASSERT_FALSE(ht.exists("c"));
 }
 
 int main(int argc, char **argv) {
